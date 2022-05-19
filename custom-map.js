ab5edba7e503a18aa599729e4abf68434a2f6a8d
Suhas Kekuda <suhaskekuda@FVFFN3A7Q05P.local>
2022-05-19 17:57:20 +0530

    added css to leaflet

--- custom-map.js
+++ custom-map.js
@@ -42,6 +42,20 @@ looker.plugins.visualizations.add({
         link.media = 'all';
         head.appendChild(link);
 
+        var link = document.createElement('link');
+        link.rel = 'stylesheet';
+        link.type = 'text/css';
+        link.href = 'https://rawgit.com/Leaflet/Leaflet.markercluster/leaflet-0.7/dist/MarkerCluster.css';
+        link.media = 'all';
+        head.appendChild(link);
+
+        var link = document.createElement('link');
+        link.rel = 'stylesheet';
+        link.type = 'text/css';
+        link.href = 'https://rawgit.com/Leaflet/Leaflet.markercluster/leaflet-0.7/dist/MarkerCluster.Default.css';
+        link.media = 'all';
+        head.appendChild(link);
+
         var elements = document.createElement("div");
         elements.setAttribute("id", "leaflet_map");
         document.getElementById("vis").appendChild(elements);
@@ -53,11 +67,35 @@ looker.plugins.visualizations.add({
     // Render in response to the data or settings changing
     update: function (data, element, settings, resp) {
 
-        var map = L.map('leaflet_map').setView([51.505, -0.09], 13);
+        var tiles = L.tileLayer('//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 18,
+            attribution: '&copy; <a href="//openstreetmap.org/copyright">OpenStreetMap</a> contributors, Points &copy 2012 LINZ'
+        });
 
-        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(map);
+        var map = L.map('leaflet_map', {
+            center: L.latLng(-37.89, 175.46),
+            zoom: 13,
+            layers: [tiles]
+        });
+
+        var mcg = L.markerClusterGroup({
+            chunkedLoading: true,
+            //singleMarkerMode: true,
+            spiderfyOnMaxZoom: false
+        });
+
+        for (var i = 0; i < addressPoints.length; i++) {
+            var a = addressPoints[i];
+            var title = a[2];
+            var marker = L.marker(new L.LatLng(a[0], a[1]), { title: title });
+            marker.bindPopup(title);
+            mcg.addLayer(marker);
+        }
+
+        map.addLayer(mcg);
+
+
+
 
     }
 });
